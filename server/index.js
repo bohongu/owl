7fd062927d1d35dba7e091b646c5cfd599b76d7e
bohongu <jaehong2782@naver.com>
2022-11-13 16:33:51 +0900

    be setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
-
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
 const app = express();
 
-app.listen(3001, () => {
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+server.listen(3001, () => {
   console.log(`SERVER IS RUNNING ON http://localhost:3001`);
 });
