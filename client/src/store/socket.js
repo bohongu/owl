b4dccb2a3553a684dddf09ed4f8975d91f4396cf
bohongu <jaehong2782@naver.com>
2022-11-29 11:23:08 +0900

    connect

--- client/src/store/socket.js
+++ client/src/store/socket.js
@@ -1,7 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { io } from 'socket.io-client';
 
-const initialState = { socket: io.connect('http://localhost:3001') };
+const initialState = {
+  socket: io.connect('https://owl-chat-app.herokuapp.com/'),
+};
 
 const socketSlice = createSlice({
   name: 'socket',
